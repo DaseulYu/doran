a225ee553268a454d0cbfbb900b8da8a76784f72
jdy1025 <jdt1025@naver.com>
2022-06-17 09:03:22 +0900

    가입 신청 회원 목록 조회

--- src/main/java/semi/member/community/controller/SelectCommunityMemberServlet.java
+++ src/main/java/semi/member/community/controller/SelectCommunityMemberServlet.java
@@ -20,12 +20,12 @@ import semi.member.member.model.vo.Member;
 @WebServlet("/community/admin/select")
 public class SelectCommunityMemberServlet extends HttpServlet{
 	
-//	@Override
-//	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//		
-//		String path = "/WEB-INF/views/community/meeting-admin.jsp";
-//		req.getRequestDispatcher(path).forward(req, resp);
-//	}
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String path = "/WEB-INF/views/community/meeting-admin.jsp";
+		req.getRequestDispatcher(path).forward(req, resp);
+	}
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
